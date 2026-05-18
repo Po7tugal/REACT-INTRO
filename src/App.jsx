a7c7f7ffc162b7921edef6b8eafc4d545feaa0d1
Filipe <a14812@oficina.pt>
2026-05-18 09:40:16 +0000

    att

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,8 @@ function App() {
           onClick={() => setCount((count) => count + 1)}
         >
           Count is {count}
+          <br />
+          new Count is {count}
         </button>
       </section>
 
