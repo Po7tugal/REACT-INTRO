9504aa431f45509b6e2b33791cf13a6f8e0d5979
Filipe <a14812@oficina.pt>
2026-05-21 09:37:14 +0000

    Intro Boostrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 // import Message from './Message.jsx'
 import App from './App.jsx'
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
+//import Header from './Header.jsx'
+//import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
     <App />
-    <Footer />
   </StrictMode>,
 )
